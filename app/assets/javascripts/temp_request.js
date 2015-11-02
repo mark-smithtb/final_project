af4abaa3b42852bd763b39cafb52cccfd4ccac44
msmith46234 <msmith46234@yahoo.com>
2015-11-02 14:21:33 -0500

    changed gradient for safari support

--- app/assets/javascripts/temp_request.js
+++ app/assets/javascripts/temp_request.js
@@ -8,29 +8,29 @@ $(function() {
       $( ".circle" ).val( ui.value );
       var x = $( ".circle" ).val();
       if (x <= 60) {
-        $(".circle").css("background", "radial-gradient(circle at 100px 100px, rgb(77, 166, 207), rgb(-40, 80, 107)");
+        $(".circle").css("background", "-webkit-radial-gradient(circle at 100px 100px, rgb(77, 166, 207), rgb(-40, 80, 107)");
         // -117, -80, -100
       }
       else if (x > 60 && x <= 65) {
-        $(".circle").css("background", "radial-gradient(circle at 100px 100px, rgb(90, 197, 207), rgb(-27, 117, 107)");
+        $(".circle").css("background", "-webkit-radial-gradient(circle at 100px 100px, rgb(90, 197, 207), rgb(-27, 117, 107)");
       }
       else if (x > 65 && x <= 70) {
-        $(".circle").css("background", "radial-gradient(circle at 100px 100px, rgb(125, 215, 191), rgb(8, 135, 91)");
+        $(".circle").css("background", "-webkit-radial-gradient(circle at 100px 100px, rgb(125, 215, 191), rgb(8, 135, 91)");
       }
       else if (x > 70 && x <= 75) {
-        $(".circle").css("background", "radial-gradient(circle at 100px 100px, #b1cfa1, #3c773d)");
+        $(".circle").css("background", "-webkit-gradient(circle at 100px 100px, #b1cfa1, #3c773d)");
       }
       else if (x > 75 && x <= 80) {
-        $(".circle").css("background", "radial-gradient(circle at 100px 100px, rgb(230, 191, 124), rgb(113, 111, 24)");
+        $(".circle").css("background", "-webkit-gradient(circle at 100px 100px, rgb(230, 191, 124), rgb(113, 111, 24)");
       }
       else if (x > 80 && x <= 85) {
-        $(".circle").css("background", "radial-gradient(circle at 100px 100px, rgb(215, 145, 105), rgb(98, 65, 5)");
+        $(".circle").css("background", "-webkit-radial-gradient(circle at 100px 100px, rgb(215, 145, 105), rgb(98, 65, 5)");
       }
       else if (x > 85 && x <= 90) {
-        $(".circle").css("background", "radial-gradient(circle at 100px 100px, rgb(205, 113, 89), rgb(88, 33, -11)");
+        $(".circle").css("background", "-webkit-radial-gradient(circle at 100px 100px, rgb(205, 113, 89), rgb(88, 33, -11)");
       }
       else if (x > 90) {
-        $(".circle").css("background", "radial-gradient(circle at 100px 100px, rgb(196, 70, 58), rgb(79, -10, -42)");
+        $(".circle").css("background", "-webkit-radial-gradient(circle at 100px 100px, rgb(196, 70, 58), rgb(79, -10, -42)");
       };
     }
   });
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-def room_create
-  room_array = (1..100).to_a
-room = Room.create(room_number: room_array.sample)
-end
+guest = Guest.create(name: 'Gavin', phone_number: "727-389-4944", reservation_number: "123")
+room_assignment = guest.room_assignments.create
+room = Room.create(room_number: 1)
+room_assignment.update(room_id: room.id)
 
-if Room.count < 50
-  50.times.map {room_create}
-end
 
-def guest_create
-guest = Guest.create(name: Faker::Name.name, phone_number: Faker::PhoneNumber.phone_number, reservation_number: Faker::Number.number(5) )
-guest.room_assignments.create
-end
-
-if Guest.count < 40
-  40.times.map {guest_create}
-end
+# def room_create
+#   room_number = 2
+# room = Room.create(room_number: room_number =+ 1)
+# end
+#
+# if Room.count < 50
+#   50.times.map {room_create}
+# end
+#
+# def guest_create
+# guest = Guest.create(name: Faker::Name.name, phone_number: Faker::PhoneNumber.phone_number, reservation_number: Faker::Number.number(5) )
+# guest.room_assignments.create
+# end
+#
+# if Guest.count < 40
+#   40.times.map {guest_create}
+# end
